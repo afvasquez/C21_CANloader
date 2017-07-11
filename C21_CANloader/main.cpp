4c046d8f4a87c1cda08616c1ec6d7a9de81397df
Andres Vasquez <andres.vasquez.ee@gmail.com>
2017-07-11 16:38:17 -0400

    Start of project

---
+++ C21_CANloader/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * C21_CANloader.cpp
+ *
+ * Created: 7/11/2017 4:03:26 PM
+ * Author : avasquez
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
